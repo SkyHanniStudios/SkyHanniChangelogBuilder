cd70106d4e4212242869ec6fcf28575a616c29a0
hannibal2 <24389977+hannibal00212@users.noreply.github.com>
2024-07-15 21:06:43 +0200

    updated to 0.26 beta 17

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -39,10 +39,10 @@ enum class WhatToDo {
 }
 
 fun main() {
-    val firstPr = 2197
+    val firstPr = 2203
     val hideWhenError = true
     val fullVersion = "0.26"
-    val beta = 16
+    val beta = 17
 
     val whatToDo = WhatToDo.NEXT_BETA
 //    val whatToDo = WhatToDo.OPEN_PRS
