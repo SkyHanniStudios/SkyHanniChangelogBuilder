e006a18ab3de0efaf92df0819e09ebb76ddd4c22
calwolfson <cwolfson58@gmail.com>
2024-12-22 07:53:07 -0500

    that can be lowercase

--- src/main/kotlin/at/hannibal2/changelog/SkyHanniChangelogBuilder.kt
+++ src/main/kotlin/at/hannibal2/changelog/SkyHanniChangelogBuilder.kt
@@ -302,7 +302,7 @@ object SkyHanniChangelogBuilder {
         for (category in PullRequestCategory.entries) {
             if (type == TextOutputType.DISCORD_PUBLIC && category == PullRequestCategory.INTERNAL) continue
 
-            val relevantChanges = changes.filter { it.category == category }.sortedBy { it.text }
+            val relevantChanges = changes.filter { it.category == category }.sortedBy { it.text.lowercase() }
             if (relevantChanges.isEmpty()) continue
             list.add("### " + category.changelogName)
             if (type == TextOutputType.DISCORD_PUBLIC) {
@@ -400,8 +400,8 @@ fun main() {
      * Will be in the format of full version, beta version
      */
     // todo allow this to work with full versions
-    val specificPreviousVersion: UpdateVersion? = null
-//    val specificPreviousVersion: UpdateVersion? = UpdateVersion("0.28", "21")
+//    val specificPreviousVersion: UpdateVersion? = null
+    val specificPreviousVersion: UpdateVersion? = UpdateVersion("0.28", "21")
 
     var whatToFetch = WhatToFetch.ALREADY_MERGED
 
